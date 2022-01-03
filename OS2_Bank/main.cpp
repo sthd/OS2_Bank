999977ace111d8889bb7a89ad621a298bd9730e0
elior <sch@campus.technion.ac.il>
2022-01-03 13:52:34 +0200

    Initial Commit

---
+++ OS2_Bank/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OS2_Bank
+//
+//  Created by Elior on 03/01/2022.
+//  Copyright © 2022 Elior. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
